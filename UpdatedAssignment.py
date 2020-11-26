44d084d2a6ae46902bb3afc29afc725cdb5837a9
Jakob Johansson <jakobtj@hotmail.com>
2020-11-26 18:55:58 +0100

    added an extra method to print out text

--- UpdatedAssignment.py
+++ UpdatedAssignment.py
@@ -11,3 +11,16 @@ def mul(x: float, y: float):
 def div(x: float, y: float):
     return x / y
 
+def print_out_text(sum):
+    if sum < 50:
+        print("less than fifty")
+    elif sum == 50:
+        print("Fifty")
+    elif 50 < sum < 100:
+        print("Almost a hundred")
+    elif sum == 100:
+        print("Spot on")
+    elif sum > 100:
+        print("Missed the spot!")
+    else:
+        print("negative number")
