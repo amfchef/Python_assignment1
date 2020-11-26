b71aa4efa18c5fa17e6e339134d7011b7ef7938e
Jakob Johansson <jakobtj@hotmail.com>
2020-11-26 18:53:24 +0100

    first commit, adding methods

---
+++ UpdatedAssignment.py
@@ -0,0 +1,13 @@
+def add(x: float, y: float):
+    print(f"{x} {y}")
+    return x + y
+
+def sub(x: float, y: float):
+    return x - y
+
+def mul(x: float, y: float):
+    return x * y
+
+def div(x: float, y: float):
+    return x / y
+
